b45445993133e838f832d46539e776ef4c3042ca
Yong <yzeng@uhnresearch.ca>
2025-01-16 17:48:31 -0500

    V1.0.0._beta

--- test/sbatch_Snakefile_lymphoma_14k.sh
+++ test/sbatch_Snakefile_lymphoma_14k.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -p all              ## Specify SLURM partition for job submssion
-#SBATCH -t 2-00:00:00
+#SBATCH -t 5-00:00:00
 #SBATCH --mem=10G
 #SBATCH -J submit_snakemake_%j
 #SBATCH -o submit_snakemake_%j.out
@@ -33,7 +33,7 @@ snakemake --snakefile /cluster/home/yzeng/snakemake/iSHARC/workflow/Snakefile \
           --configfile /cluster/home/yzeng/snakemake/iSHARC/test/config_lymphoma_14k.yaml \
           --cluster-config /cluster/home/yzeng/snakemake/iSHARC/workflow/config/cluster_std_err.json \
           --keep-going  --use-conda  --conda-prefix ${CONDA_PREFIX}_extra_env \
-          --cluster "sbatch -p veryhimem -c 12 --mem=100G -J {cluster.jid} -o {cluster.std} -e {cluster.err} -t 2-00:00:00" \
+          --cluster "sbatch -p veryhimem -c 12 --mem=100G -J {cluster.jid} -o {cluster.std} -e {cluster.err} -t 5-00:00:00" \
           --latency-wait 60 --jobs 1 -p
 
 # Using Higher Memory: For large dataset integration, allocate more memory (e.g., --mem=600G).
